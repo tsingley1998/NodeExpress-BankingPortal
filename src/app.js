97699c8083851a1556641446180ddcc739e7e11d
Tori Singley <toribirdy1998@gmail.com>
2020-07-26 20:53:31 -0600

    part 3

--- src/app.js
+++ src/app.js
@@ -41,11 +41,11 @@ app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
     const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accounts.JSON, 'utf8');
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
     res.render('transfer', { message: 'Transfer Completed' });
 });
 
-app.get('/payment', (req, ers) => {
+app.get('/payment', (req, res) => {
     res.render('payment', { account: accounts.credit });
 });
 app.post('/payment', (req, res) => {
@@ -54,7 +54,7 @@ app.post('/payment', (req, res) => {
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
     res.render('payment', { message: 'Payment Successful', account: accounts.credit });
-    
+
 });
 
 app.get('/profile', (req, res) => {
