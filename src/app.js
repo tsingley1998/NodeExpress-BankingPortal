b1be3c1d4946ab38d99afee0e086fb48c9019f53
Tori Singley <toribirdy1998@gmail.com>
2020-07-26 20:49:51 -0600

    payment part2

--- src/app.js
+++ src/app.js
@@ -37,6 +37,25 @@ app.get('/credit', (req, res) => {
 app.get('/transfer', (req, res) => {
     res.render('transfer');
 });
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accounts.JSON, 'utf8');
+    res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', (req, ers) => {
+    res.render('payment', { account: accounts.credit });
+});
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+    
+});
 
 app.get('/profile', (req, res) => {
     res.render('profile', { user: users[0]});
